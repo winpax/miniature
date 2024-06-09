087107c84e5e0deeeac8f073e35914c4c057bb40
Juliette Cordor <professional@maybejules.com>
2024-06-09 21:36:05 +1000

    add statics

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,42 @@ extern crate alloc;
 
 use std::{
     error::Error,
+    ffi::OsString,
     fs::File,
     io::{BufRead, BufReader, Read},
     path::PathBuf,
 };
+
+use libc::wchar_t;
+
+const MAX_PATH: usize = windows::Win32::Foundation::MAX_PATH as usize + 2;
+
+#[cfg(not(debug_assertions))]
+#[no_mangle]
+#[link_section = ".shim_path"]
+pub static PATH: [wchar_t; MAX_PATH] = [0; MAX_PATH];
+
+#[cfg(debug_assertions)]
+pub static PATH: [wchar_t; MAX_PATH] = [
+    67, 58, 92, 117, 115, 101, 114, 115, 92, 106, 117, 108, 105, 101, 92, 115, 99, 111, 111, 112,
+    92, 97, 112, 112, 115, 92, 115, 102, 115, 117, 92, 99, 117, 114, 114, 101, 110, 116, 92, 115,
+    102, 115, 117, 46, 101, 120, 101, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0,
+];
+
+#[no_mangle]
+#[link_section = ".shim_args"]
+pub static ARGS: [wchar_t; MAX_PATH] = [0; MAX_PATH];
+
+#[no_mangle]
+#[link_section = ".shim_command"]
+pub static COMMAND: [wchar_t; 256] = [0; 256];
 
 fn ctrl_handler(ctrl_type: u32) -> bool {
     use windows::Win32::System::Console::{
@@ -27,93 +59,6 @@ fn ctrl_handler(ctrl_type: u32) -> bool {
 }
 
 fn _main() -> Result<(), Box<dyn Error>> {
-    #[cfg(not(debug_assertions))]
-    let filename = {
-        let current_exe = std::env::current_exe().unwrap();
-
-        current_exe.with_extension("shim")
-    };
-
-    #[cfg(debug_assertions)]
-    let filename = PathBuf::from("test.shim");
-
-    let file = File::open(&filename)?;
-
-    let mut command_length = 256;
-    let mut path_length = 0;
-    let mut args_length = 0;
-
-    for line in BufReader::new(file).lines() {
-        let line = line?;
-
-        if &line[4..6] == " = " {
-            continue;
-        }
-
-        let linelen = line.len();
-        let mut len =
-            linelen - 8 + usize::from(line.chars().nth(linelen - 1).is_some_and(|c| c != '\n'));
-
-        dbg!(len);
-
-        if &line[0..4] == "path" {
-            let add_quotes = if let Some(quote) = line.chars().nth(7)
-                && quote == '"'
-            {
-                let mut add_quotes = false;
-
-                for i in 7..len {
-                    if line.chars().nth(i) == Some(' ') {
-                        add_quotes = true;
-                        break;
-                    }
-                }
-
-                add_quotes
-            } else {
-                false
-            };
-
-            if add_quotes {
-                len += 2;
-            }
-
-            let mut path = String::with_capacity(len + 1);
-
-            if add_quotes {
-                path.push('"');
-            }
-
-            path.push_str(&line[7..(if add_quotes { len - 1 } else { len })]);
-
-            if add_quotes {
-                path.push('"');
-            }
-
-            command_length += len;
-            path_length += len;
-
-            println!("{path}");
-
-            continue;
-        }
-
-        if &line[0..4] == "args" {
-            let args = line[7..].to_string();
-
-            command_length += args.len() + 1;
-            args_length = args.len() + 1;
-        }
-
-        unsafe { libc::printf(line.as_ptr().cast()) };
-    }
-
-    #[cfg(debug_assertions)]
-    unsafe {
-        let skinny_filename = filename.display().to_string();
-        libc::printf(skinny_filename.as_ptr().cast());
-    }
-
     Ok(())
 }
 
