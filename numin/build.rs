ee2840d61f12c4955db50d5510e7f0dcd0334270
Juliette Cordor <me@cordor.dev>
2025-04-10 13:48:41 +1000

    refactored build script

--- numin/build.rs
+++ numin/build.rs
@@ -4,7 +4,7 @@ fn handle_error<T, E: Error>(result: Result<T, E>) -> T {
     match result {
         Err(e) => {
             println!("cargo::error={}", e);
-            std::process::exit(1);
+            std::process::exit(0);
         }
         Ok(v) => v,
     }
@@ -17,9 +17,14 @@ fn main() {
         "/miniature.exe"
     );
 
-    let binary_data =
-        handle_error(reqwest::blocking::get(download_url).and_then(|resp| resp.bytes()));
+    let resp = handle_error(reqwest::blocking::get(download_url));
+    if !resp.status().is_success() {
+        println!("cargo::error=Failed to download miniature.exe");
+        println!("cargo::error={}", resp.status());
+    } else {
+        let binary_data = handle_error(resp.bytes());
 
-    let output_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("miniature.exe");
-    fs::write(&output_path, &binary_data).expect("Failed to write miniature.exe");
+        let output_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("miniature.exe");
+        fs::write(&output_path, &binary_data).expect("Failed to write miniature.exe");
+    }
 }
