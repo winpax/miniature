a9d808fe60aaac8538f811904064eac505101b52
Juliette Cordor <me@cordor.dev>
2025-04-11 21:29:04 +1000

    rerun build on changed c files

--- common/build.rs
+++ common/build.rs
@@ -1,9 +1,11 @@
 fn main() {
     let mut build = cc::Build::new();
     build.file("interop/macros.c");
+    println!("cargo:rerun-if-changed=interop/macros.c");
 
     #[cfg(feature = "crt_functions")]
     build.file("interop/crt_fns.c");
+    println!("cargo:rerun-if-changed=interop/crt_fns.c");
 
     build.compile("interop");
     println!("cargo:rerun-if-changed=interop.c");
