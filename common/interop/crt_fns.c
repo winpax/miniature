2f2f85330e41055b3a7459ad6c6b7cf19f5aee26
Juliette Cordor <me@cordor.dev>
2025-08-05 19:47:34 +1000

    added aulldiv and aullrem crt functions to interop

--- common/interop/crt_fns.c
+++ common/interop/crt_fns.c
@@ -2,5 +2,16 @@
 
 int _fltused = 0;
 
-extern __chkstk(size_t ptr) {
+extern __chkstk(size_t ptr)
+{
+}
+
+extern _aullrem(unsigned long long a, unsigned long long b)
+{
+    return a % b;
+}
+
+extern _aulldiv(unsigned long long a, unsigned long long b)
+{
+    return a / b;
 }
